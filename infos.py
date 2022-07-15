7c2eeadde857caefcfa52c034b737221ab32b1e7
SokyShop <108899509+SokyFre2@users.noreply.github.com>
2022-07-14 22:58:31 -0400

    Update infos.py

--- infos.py
+++ infos.py
@@ -91,7 +91,7 @@ def createFinishUploading(filename,filesize,split_size,current,count,findex):
 def createFileMsg(filename,files):
     import urllib
     if len(files)>0:
-        msg= '<b>⇲ Descargar archivo ⋐⋑</b>\n'
+        msg= '<b>⇲ Descargar archivo </b>\n'
         for f in files:
             url = urllib.parse.unquote(f['directurl'],encoding='utf-8', errors='replace')
             #msg+= '<a href="'+f['url']+'">➥' + f['name'] + '⋐⋑</a>'
