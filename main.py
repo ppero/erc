4afc14e38257da68b6942e739543ff9fb1e25a8d
SokyShop <108899509+SokyFre2@users.noreply.github.com>
2022-07-15 20:21:05 -0400

    Update main.py

--- main.py
+++ main.py
@@ -476,7 +476,7 @@ def onmessage(update,bot:ObigramClient):
         thread.store('msg',message)
 
         if '/start' in msgText:
-            start_msg = '➥ @UplodPublicBot soy un bot público Puedo subir a cualquier nube , también puedo descargar ⎙ archivos de ➫ Mega , ➫ Google Drive , ➫ YouTube , ➫ Link directo\n'
+            start_msg = '👋 Hola {NAME} soy un bot público puedo subir a cualquier nube , también puedo descargar ⎙ archivos de ➫ Mega , ➫ Google Drive , ➫ YouTube , ➫ Link directo\n'
             bot.editMessageText(message,start_msg)
         elif '/files' == msgText and user_info['cloudtype']=='moodle':
              proxy = ProxyCloud.parse(user_info['proxy'])
